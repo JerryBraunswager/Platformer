bfc7f95869d893888d62b65e34b9839b9b68759a
JerryBraunswager <80885679+JerryBraunswager@users.noreply.github.com>
2024-01-03 23:43:44 +0300

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform _startPosition;
+    [SerializeField] private Transform _endPosition;
+    [SerializeField] private float _speed;
+
+    private bool _isEndPositionAchieved = false;
+    private bool _isRight;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector3 target;
+
+        if (_isEndPositionAchieved == false)
+        {
+            target = _endPosition.position;
+            _isRight = true;
+
+            if (transform.position.x == _endPosition.position.x)
+            {
+                _isEndPositionAchieved = true;
+            }
+        }
+        else
+        {
+            target = _startPosition.position;
+            _isRight = false;
+
+            if (transform.position == _startPosition.position)
+            {
+                _isEndPositionAchieved = false;
+            }
+        }
+
+        _animator.SetBool(nameof(_isRight), _isRight);
+        transform.position = Vector3.MoveTowards(transform.position, target, _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private float _currentSpeed;
+    private float _direction;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        _direction = Input.GetAxis("Horizontal");
+        _animator.SetFloat(nameof(_direction), _direction);
+        _rigidbody.velocity = new Vector2(_direction * Time.deltaTime * _speed, Input.GetAxis("Jump") * _jumpForce);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+}
